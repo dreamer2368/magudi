c63a2c68752145c3be826c8f81ec37b6f9c3ac7c
Ramanathan Vishnampet <rvishnampet@gmail.com>
2015-04-03 15:47:03 -0500

    Added functionality to accept a range of timesteps in vorticity dilatation calculation.

--- utils/vorticity_dilatation.f90
+++ utils/vorticity_dilatation.f90
@@ -17,8 +17,8 @@ program vorticity_dilatation
 
   implicit none
 
-  integer :: i, procRank, ierror
-  character(len = STRING_LENGTH) :: filename, message, programName
+  integer :: i, j, startTimestep, endTimestep, saveInterval, procRank, ierror
+  character(len = STRING_LENGTH) :: filename, message, programName, outputPrefix
   logical :: success
   type(t_Region) :: region
   integer, allocatable :: globalGridSizes(:,:)
@@ -41,34 +41,59 @@ program vorticity_dilatation
   call region%setup(MPI_COMM_WORLD, globalGridSizes)
   call region%loadData(QOI_GRID, filename)
 
-  ! Load the solution file.
-  if (command_argument_count() >= 1) then
-     call get_command_argument(1, filename)
-  else
-     call getRequiredOption("solution_file", filename)
-  end if
-  call region%loadData(QOI_FORWARD_STATE, filename)
-
   ! Compute normalized metrics, norm matrix and Jacobian.
   do i = 1, size(region%grids)
      call region%grids(i)%update()
   end do
   call MPI_Barrier(MPI_COMM_WORLD, ierror)
 
-  ! Compute velocity
-  do i = 1, size(region%grids)
-     call computeDependentVariables(size(globalGridSizes, 1),                                &
-          region%states(i)%conservedVariables, velocity = region%states(i)%velocity)
-  end do
+  if (command_argument_count() >= 1) then !... only one solution file to process.
 
-  ! Save vorticity and dilatation
-  i = len_trim(filename)
-  if (filename(i-1:i) == ".q") then
-     filename = filename(:i-2) // ".vorticity_dilatation.f"
+     ! Load the solution file.
+     call get_command_argument(1, filename)
+     call region%loadData(QOI_FORWARD_STATE, filename)
+
+     ! Compute velocity
+     do i = 1, size(region%grids)
+        call computeDependentVariables(size(globalGridSizes, 1),                                &
+             region%states(i)%conservedVariables, velocity = region%states(i)%velocity)
+     end do
+     
+     ! Save vorticity and dilatation
+     i = len_trim(filename)
+     if (filename(i-1:i) == ".q") then
+        filename = filename(:i-2) // ".vorticity_dilatation.f"
+     else
+        filename = PROJECT_NAME // ".vorticity_dilatation.f"
+     end if
+     call region%saveData(QOI_VORTICITY_DILATATION, filename)
+
   else
-     filename = PROJECT_NAME // ".vorticity_dilatation.f"
+
+     call getRequiredOption("save_interval", saveInterval)
+     call getRequiredOption("start_timestep", startTimestep)
+     call getRequiredOption("end_timestep", endTimestep)
+
+     outputPrefix = getOption("output_prefix", PROJECT_NAME)
+     
+     do i = startTimestep, endTimestep, saveInterval
+
+        write(filename, '(2A,I8.8,A)') trim(outputPrefix), "-", i, ".q"
+        call region%loadData(QOI_FORWARD_STATE, filename)
+
+        ! Compute velocity
+        do j = 1, size(region%grids)
+           call computeDependentVariables(size(globalGridSizes, 1),                          &
+                region%states(j)%conservedVariables, velocity = region%states(j)%velocity)
+        end do
+     
+        ! Save vorticity and dilatation
+        write(filename, '(2A,I8.8,A)') trim(outputPrefix), "-", i, "..vorticity_dilatation.f"
+        call region%saveData(QOI_VORTICITY_DILATATION, filename)        
+        
+     end do
+
   end if
-  call region%saveData(QOI_VORTICITY_DILATATION, filename)
 
   ! Cleanup.
   call region%cleanup()
