0b6c06d29a4e643e317f67dadd14caabe5b57474
Jesse Capecelatro <jcaps@jessecaps.csl.illinois.edu>
2015-05-21 13:29:50 -0500

    Incoming inviscid flux Jacobian test now implemented for multiple species.

--- test/incoming_inviscid_flux_jacobian.f90
+++ test/incoming_inviscid_flux_jacobian.f90
@@ -8,14 +8,15 @@ program incoming_inviscid_flux_jacobian
 
   implicit none
 
-  integer, parameter :: wp = SCALAR_KIND, n = 2 ** 10
+  integer, parameter :: wp = SCALAR_KIND, n = 2 ** 10, maxSpecies = 6
   real(wp), parameter :: ratioOfSpecificHeats = 1.4_wp
   logical :: success
-  integer :: i, j, nDimensions
+  integer :: i, j, k, nDimensions, nSpecies
   SCALAR_TYPE, allocatable :: conservedVariables(:,:), metrics(:,:), specificVolume(:),      &
-       velocity(:,:), temperature(:), localConservedVariables(:), localVelocity(:),          &
-       localMetricsAlongDirection(:), jacobianOfInviscidFlux(:,:),                           &
-       incomingJacobianOfInviscidFlux1(:,:), incomingJacobianOfInviscidFlux2(:,:)
+       velocity(:,:), temperature(:), massFraction(:,:), localConservedVariables(:),         &
+       localVelocity(:), localMassFraction(:), localMetricsAlongDirection(:),                &
+       jacobianOfInviscidFlux(:,:), incomingJacobianOfInviscidFlux1(:,:),                    &
+       incomingJacobianOfInviscidFlux2(:,:)
   logical :: isDomainCurvilinear
 
   interface
@@ -36,19 +37,30 @@ program incoming_inviscid_flux_jacobian
   call initializeErrorHandler()
   call initializeRandomNumberGenerator()
 
-  do nDimensions = 1, 3
+  nSpecies = random(0, maxSpecies)
 
-     allocate(conservedVariables(n, nDimensions + 2))
+  print *
+  print *, 'Number of species:',nSpecies
+  print *
+
+  do nDimensions = 3, 3
+
+     allocate(conservedVariables(n, nDimensions + 2 + nSpecies))
      allocate(metrics(n, nDimensions ** 2))
      allocate(specificVolume(n))
      allocate(velocity(n, nDimensions))
      allocate(temperature(n))
-     allocate(localConservedVariables(nDimensions + 2))
+     allocate(massFraction(n, nSpecies))
+     allocate(localConservedVariables(nDimensions + 2 + nSpecies))
      allocate(localVelocity(nDimensions))
+     allocate(localMassFraction(nSpecies))
      allocate(localMetricsAlongDirection(nDimensions))
-     allocate(jacobianOfInviscidFlux(nDimensions + 2, nDimensions + 2))
-     allocate(incomingJacobianOfInviscidFlux1(nDimensions + 2, nDimensions + 2))
-     allocate(incomingJacobianOfInviscidFlux2(nDimensions + 2, nDimensions + 2))
+     allocate(jacobianOfInviscidFlux                                                         &
+          (nDimensions + 2 + nSpecies, nDimensions + 2 + nSpecies))
+     allocate(incomingJacobianOfInviscidFlux1                                                &
+          (nDimensions + 2 + nSpecies, nDimensions + 2 + nSpecies))
+     allocate(incomingJacobianOfInviscidFlux2                                                &
+          (nDimensions + 2 + nSpecies, nDimensions + 2 + nSpecies))
 
      isDomainCurvilinear = (random(0, 1) == 0)
 
@@ -62,6 +74,10 @@ program incoming_inviscid_flux_jacobian
              random(0.1_wp, 4.0_wp) / ratioOfSpecificHeats +                                 &
              0.5_wp / conservedVariables(i,1) *                                              &
              sum(conservedVariables(i,2:nDimensions+1) ** 2)
+        do k = 1, nSpecies
+           conservedVariables(i, nDimensions + 2 + k) = conservedVariables(i,1) *            &
+                random(0.0_wp, 1.0_wp)
+        end do
 
         assert(conservedVariables(i,1) > 0.0_wp)
 
@@ -78,30 +94,34 @@ program incoming_inviscid_flux_jacobian
 
      end do
 
-     call computeDependentVariables(nDimensions, 0, conservedVariables,                      &
+     call computeDependentVariables(nDimensions, nSpecies, conservedVariables,               &
           ratioOfSpecificHeats, specificVolume = specificVolume,                             &
-          velocity = velocity, temperature = temperature)
+          velocity = velocity, temperature = temperature, massFraction = massFraction)
      assert(all(specificVolume > 0.0_wp))
      assert(all(temperature > 0.0_wp))
+     assert(all(massFraction >= 0.0_wp))
+     assert(all(massFraction <= 1.0_wp))
 
      do i = 1, n
 
         localConservedVariables = conservedVariables(i,:)
         localVelocity = velocity(i,:)
+        localMassFraction = massFraction(i,:)
 
         do j = 1, nDimensions
 
            localMetricsAlongDirection = metrics(i,1+nDimensions*(j-1):nDimensions*j)
 
-           call computeJacobianOfInviscidFlux(nDimensions, 0, localConservedVariables,       &
-                   localMetricsAlongDirection, ratioOfSpecificHeats,                         &
-                   jacobianOfInviscidFlux, specificVolume = specificVolume(i),               &
-                   velocity = localVelocity, temperature = temperature(i))
-              call computeIncomingJacobianOfInviscidFlux(nDimensions, 0,                     &
-                   localConservedVariables, localMetricsAlongDirection,                      &
-                   ratioOfSpecificHeats, 1, incomingJacobianOfInviscidFlux1,                 &
-                   specificVolume = specificVolume(i), velocity = localVelocity,             &
-                   temperature = temperature(i))
+           call computeJacobianOfInviscidFlux(nDimensions, nSpecies,                         &
+                localConservedVariables, localMetricsAlongDirection,                         &
+                ratioOfSpecificHeats, jacobianOfInviscidFlux,                                &
+                specificVolume = specificVolume(i), velocity = localVelocity,                &
+                temperature = temperature(i), massFraction = localMassFraction)
+           call computeIncomingJacobianOfInviscidFlux(nDimensions, nSpecies,              &
+                localConservedVariables, localMetricsAlongDirection,                      &
+                ratioOfSpecificHeats, 1, incomingJacobianOfInviscidFlux1,                 &
+                specificVolume = specificVolume(i), velocity = localVelocity,             &
+                temperature = temperature(i), massFraction = localMassFraction)
 
            call lapackComputeIncomingJacobianOfInviscidFlux(jacobianOfInviscidFlux, 1,       &
                 incomingJacobianOfInviscidFlux2)
@@ -121,8 +141,10 @@ program incoming_inviscid_flux_jacobian
      SAFE_DEALLOCATE(incomingJacobianOfInviscidFlux1)
      SAFE_DEALLOCATE(jacobianOfInviscidFlux)
      SAFE_DEALLOCATE(localMetricsAlongDirection)
+     SAFE_DEALLOCATE(localMassFraction)
      SAFE_DEALLOCATE(localVelocity)
      SAFE_DEALLOCATE(localConservedVariables)
+     SAFE_DEALLOCATE(massFraction)
      SAFE_DEALLOCATE(temperature)
      SAFE_DEALLOCATE(velocity)
      SAFE_DEALLOCATE(specificVolume)
