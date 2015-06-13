a37c66b206be9589bb582b556a4d647f89360568
Ramanathan Vishnampet <rvishnampet@gmail.com>
2015-06-12 20:46:53 -0500

    Added a post-process utility for jet.

---
+++ examples/OSUMach1.3/postprocess.py
@@ -0,0 +1,28 @@
+import plot3dnasa as p3d
+import numpy as np
+
+def extract_yz(g, f):
+    n = g.get_size()
+    ge = p3d.Grid().set_size([n[0][2], n[4][0] + n[0][1] + n[2][0] - 2, 1],
+                             True)
+    if type(f) == p3d.Solution:
+        fe = p3d.Solution().copy_from(ge)
+    else:
+        fe = p3d.Function(ncomponents=f.ncomponents).copy_from(ge)
+    si = {4: [slice(None, None, -1), 0, slice(None)],
+          0: [0, slice(None), slice(None)],
+          2: [slice(None), 0, slice(None)]}
+    so = {4: [slice(None), slice(None, n[4][0] - 1), 0],
+          0: [slice(None), slice(n[4][0] - 1, n[4][0] + n[0][1] - 1), 0],
+          2: [slice(None), slice(n[4][0] + n[0][1] - 1, None), 0]}
+    starts = {4: [1, n[4][1]/2, 0], 0: [n[0][0]/2, 0, 0], 2: [1, n[2][1]/2, 0]}
+    ends = {4: [-1, n[4][1]/2, -1], 0: [n[0][0]/2, -1, -1],
+            2: [-1, n[2][1]/2, -1]}
+    for i in [4, 0, 2]:
+        g.set_subzone(i, starts[i], ends[i])
+        f.subzone_from(g)
+        for j in range(2):
+            ge.xyz[0][so[i] + [j]] = g.load().xyz[0][si[i] + [2-j]].T
+        for j in range(fe[0].shape[-1]):
+            fe[0][so[i] + [j]] = f.load()[0][si[i] + [j]].T
+    return ge, fe
