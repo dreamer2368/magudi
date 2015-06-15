43317c803b109a1251bdfe1743e44d48cc4a4ec3
Jesse Capecelatro <jessecaps@gmail.com>
2015-06-15 14:00:37 -0500

    bash script to run AcousticMonopole example on Mac

---
+++ examples/AcousticMonopole/mac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export MAGUDI_MPIRUN="mpiexec -n 4"
+
+function setOption() {
+    if grep -q "$1" magudi.inp
+    then
+	sed -i "s/^.*$1.*$/$1 = $2/g" magudi.inp
+    else
+	echo "$1 = $2" >> magudi.inp
+    fi
+}
+
+rm -f AcousticMonopole* Bootstrap/AcousticMonopole*
+python config.py
+cp AcousticMonopole.xyz AcousticMonopole.target.q Bootstrap
+cd Bootstrap
+$MAGUDI_MPIRUN ../../../build/magudi
+cp AcousticMonopole-00000240.q ../AcousticMonopole.ic.q
+cd ..
+setOption "disable_adjoint_solver" true
+setOption "compute_time_average" true
+$MAGUDI_MPIRUN ../../build/magudi
+python -c "from config import *; mean_pressure(p3d.fromfile('AcousticMonopole.mean.q')).save('AcousticMonopole.mean_pressure.f')"
+setOption "disable_adjoint_solver" false
+setOption "compute_time_average" false
+$MAGUDI_MPIRUN ../../build/magudi
