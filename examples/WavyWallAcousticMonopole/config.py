1080971d09acf0744b4b7e92d240496899bd97a6
David Buchta <dbuchta@redmesa-login1.sandia.gov>
2015-06-24 11:49:10 -0600

    changes to config.py for shape mollifier along wall

--- examples/WavyWallAcousticMonopole/config.py
+++ examples/WavyWallAcousticMonopole/config.py
@@ -2,22 +2,27 @@
 import numpy as np
 import plot3dnasa as p3d
 
+def ShapeMollifierFunction(xNormalized):
+	MollifierFunction = lambda x: np.where(np.logical_or(x < 0., x > 1.), 0., np.tanh(40. * (x - 0.1)) - np.tanh(40. * (x - 0.9)))
+	Mollifier = MollifierFunction(xNormalized)
+	Mollifier /= np.trapz(Mollifier, xNormalized) # normalized to have unit area
+
+	return Mollifier
+
 def wallProfile(xNormalized, amplitude, gaussianFactor, nModes):
 
     from numpy.random import rand
 
     wallHeight = np.zeros_like(xNormalized)
 
-    shapeMollifierFunction = lambda x: np.where(np.logical_or(x < 0., x > 1.), 0., np.tanh(40. * (x - 0.1)) - np.tanh(40. * (x - 0.9)))
-    shapeMollifier = shapeMollifierFunction(xNormalized)
-    shapeMollifier /= np.trapz(shapeMollifier, xNormalized) # normalized to have unit area
-
+    shapeMollifier = ShapeMollifierFunction(xNormalized)
+    
     controlParameters = rand(nModes)
     phaseAngles = 2. * np.pi * rand(nModes)
     waveNumbers = 2. * np.pi * np.arange(1, nModes + 1)
 
     for i in range(nModes):
-        wallHeight += amplitude * shapeMollifier * controlParameters[i] * np.exp(- gaussianFactor * waveNumbers[i] ** 2) * np.cos(waveNumbers[i] * xNormalized + phaseAngles[i])
+        wallHeight += amplitude * shapeMollifier * controlParameters[i] * np.cos(waveNumbers[i] * xNormalized + phaseAngles[i])
 
     return wallHeight
 
@@ -42,21 +47,21 @@ def grid(size,xMin,xMax,yMin,yMax):
 		background_y=np.linspace(yMin,yMax,size[1])
 		xWallStart = -5.
 		xWallEnd   = 5.
-		wallHeight = wallProfile((x - xWallStart) / (xWallEnd - xWallStart), 0.05 , 2e-4, 10)
+		wallHeight = wallProfile((x - xWallStart) / (xWallEnd - xWallStart), 0.02 , 2e-4,20)
 	
 		for i in range(size[0]):
 			g.xyz[0][i,:,0,0] = x[i]
 
 		for j in range(size[1]):
 				yo=background_y[j]
-				width=1./4.
+				width=1./5.
 			
 				if s[j] <= width:
-					xi=1.*((width-(width-s[j]))/width)**0.5
+					xi=1.*((width-(width-s[j]))/width)
+					xi=np.tanh(5.*s[j]/width)
 				else:
 					xi=1.
 
-				#print "j={j},y={y},xi={xi}".format(j=j,y=yo,xi=xi)	
 				for k in range(size[0]):
 					allege_y=s[j] * yMax + (1. - s[j]) * (yMin + wallHeight[k])
 					g.xyz[0][k,j,0,1] = allege_y - xi*(allege_y-yo)
@@ -85,21 +90,26 @@ def target_mollifier(g,x_min,x_max,y_min,y_max):
         f.f[0][:,j,0,0] *= p3d.tanh_support(
             g.xyz[0][:,j,0,0], x_min, x_max, 40., 0.2)    
     for i in range(n[0]):
-        f.f[0][i,:,0,0] *= p3d.cubic_bspline_support(
-            g.xyz[0][i,:,0,1], y_min, y_max)
+        #f.f[0][i,:,0,0] *= p3d.cubic_bspline_support(
+        #    g.xyz[0][i,:,0,1], y_min, y_max)
+		f.f[0][i,:,0,0] *= p3d.tanh_support(
+        	g.xyz[0][i,:,0,1], y_min, y_max,40., 0.2)	
     return f
 
 def control_mollifier(g,x_min,x_max,y_min,y_max):
-    f = p3d.Function().copy_from(g)
-    f.f[0].fill(1.)
-    n = f.get_size(0)
-    for j in range(n[1]):
-        f.f[0][:,j,0,0] *= p3d.cubic_bspline_support(
-            g.xyz[0][:,j,0,0], x_min, x_max)
-    for i in range(n[0]):
-        f.f[0][i,:,0,0] *= p3d.cubic_bspline_support(
-            g.xyz[0][i,:,0,1], y_min, y_max)
-    return f
+	xWallStart = -5.
+	xWallEnd   = 5.
+	n = g.get_size(0)
+	x = np.linspace(x_min,x_max,n[0])
+	xNormalized=(x - xWallStart) / (xWallEnd - xWallStart)
+	shapeMollifier=ShapeMollifierFunction(xNormalized)
+
+	f = p3d.Function().copy_from(g)
+	f.f[0].fill(0.)
+
+	for i in range(n[0]):
+	   f.f[0][i,0,0,0]=shapeMollifier[i]
+	return f
 
 def mean_pressure(s):
     f = p3d.Function().copy_from(s)
@@ -115,22 +125,22 @@ if __name__ == '__main__':
 		yMin = -10.
 		yMax =  10.
 
-		g = grid([201,201],xMin,xMax,yMin,yMax)
+		g = grid([501,501],xMin,xMax,yMin,yMax)
 		g.save(outputPrefix+'.xyz')
 		initial_condition(g, u1=0.0, u2=0.0).save(outputPrefix+'.ic.q')
 		target_state(g, u1=0.0, u2=0.0, S=0.05).save(outputPrefix+'.target.q')
 
 		xMinTarget = 5.
 		xMaxTarget = 7.
-		yMinTarget = -9.
+		yMinTarget = -8.
 		yMaxTarget = 8.
 		target_mollifier(g,xMinTarget,xMaxTarget,yMinTarget,yMaxTarget).save(outputPrefix+'.target_mollifier.f')
 
-		xMinControl =  -6.
-		xMaxControl =  -4.
-		yMinControl = -8.5
-		yMaxControl =  -6.5
-		control_mollifier(g,xMinControl,xMaxControl,yMinControl,yMaxControl).save(outputPrefix+'.control_mollifier.f')
+		xMinControl =  xMin
+		xMaxControl =  xMax
+		yMinControl =  yMin
+		yMaxControl =  yMax
+		control_mollifier(g,xMin,xMax,yMin,yMax).save(outputPrefix+'.control_mollifier.f')
 
 		#IMPLEMENTING THE BOUNDARY CONDITION FILE
 
@@ -158,7 +168,7 @@ if __name__ == '__main__':
 				fp.write("inflowSponge         SPONGE                   1       1    1  %i    1   -1    1   -1\n"% (left_sponge))
 				fp.write("outflow              SAT_FAR_FIELD            1      -1   -1   -1    1   -1    1   -1\n")
 				fp.write("outflowSponge        SPONGE                   1      -1   %i   -1    1   -1    1   -1\n"% (right_sponge))
-				fp.write("wall.S               SAT_ISOTHERMAL_WALL            1       2    1   -1    1    1    1   -1\n")
+				fp.write("wall.S               SAT_SLIP_WALL            1       2    1   -1    1    1    1   -1\n")
 				fp.write("farfield.N           SAT_FAR_FIELD            1      -2    1   -1   -1   -1    1   -1\n")
 				fp.write("sponge.N             SPONGE                   1      -2    1   -1  %i   -1    1   -1\n"% (top_sponge))
 				fp.write("targetRegion         COST_TARGET              1       0  %i %i  %i  %i    1   -1\n"%(left_target,right_target,bottom_target,top_target))
