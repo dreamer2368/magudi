42e84f939b90450b7bac245e19db6e7240045036
Jesse Capecelatro <jessecaps@gmail.com>
2015-06-29 15:03:34 -0500

    Python script to plot mollifiers

--- examples/CounterflowDiffusion/generate_mollifiers.py
+++ examples/CounterflowDiffusion/generate_mollifiers.py
@@ -6,13 +6,13 @@ def target_mollifier(g):
     x_min = -20.
     x_max =  20.
     y_min = -8.
-    y_max =  0.
+    y_max =  1.
     f = p3d.Function().copy_from(g)
     f.f[0].fill(1.)
     n = f.get_size(0)
     for i in range(n[0]):
-        f.f[0][i,:,0,0] *= p3d.tanh_support(
-            g.xyz[0][i,:,0,1], y_min, y_max,20., 0.5)
+        f.f[0][i,:,0,0] *= p3d.cubic_bspline_support(
+            g.xyz[0][i,:,0,1], y_min, y_max)
     for j in range(n[1]):
         f.f[0][:,j,0,0] *= p3d.tanh_support(
             g.xyz[0][:,j,0,0], x_min, x_max, 40., 0.2)
@@ -25,17 +25,17 @@ def target_mollifier(g):
 def control_mollifier(g):
     x_min = -20.
     x_max =  20.    
-    y_min =  0.
+    y_min = -1.
     y_max =  8.
     f = p3d.Function().copy_from(g)
     f.f[0].fill(1.)
     n = f.get_size(0)
-    for j in range(n[1]):
-        f.f[0][:,j,0,0] *= p3d.tanh_support(
-            g.xyz[0][:,j,0,0], x_min, x_max, 40., 0.2)
     for i in range(n[0]):
         f.f[0][i,:,0,0] *= p3d.cubic_bspline_support(
             g.xyz[0][i,:,0,1], y_min, y_max)
+    for j in range(n[1]):
+        f.f[0][:,j,0,0] *= p3d.tanh_support(
+            g.xyz[0][:,j,0,0], x_min, x_max, 40., 0.2)
     imin, imax = p3d.find_extents(g.xyz[0][:,0,0,0], x_min, x_max)
     jmin, jmax = p3d.find_extents(g.xyz[0][0,:,0,1], y_min, y_max)
     print ('  {:<20} {:<21} {:>4d} {:>7d}' + 6 * ' {:>4d}').format(
