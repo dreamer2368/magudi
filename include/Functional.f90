340401c48621399ff2ecd005471ff4b556301f3d
Jesse Capecelatro <jessecaps@gmail.com>
2015-08-10 15:34:51 -0500

    Included runningTImeQuadrature. cost_functional.txt now outputs integrated cost functional

--- include/Functional.f90
+++ include/Functional.f90
@@ -6,7 +6,8 @@ module Functional_mod
 
   type, abstract, public :: t_Functional
 
-     SCALAR_TYPE :: cachedValue = real(0.0, SCALAR_KIND)
+     SCALAR_TYPE :: cachedValue = real(0.0, SCALAR_KIND),                                    &
+          runningTimeQuadrature = real(0.0, SCALAR_KIND)
 
    contains
 
--- src/SolverImpl.f90
+++ src/SolverImpl.f90
@@ -595,6 +595,7 @@ function runForward(this, region, actuationAmount, controlIteration, restartFile
   if (.not. region%simulationFlags%predictionOnly) then
      call this%functionalFactory%connect(functional)
      assert(associated(functional))
+     functional%runningTimeQuadrature = 0.0_wp
   end if
 
   ! Setup residual manager if this is a steady-state simulation.
@@ -656,7 +657,7 @@ function runForward(this, region, actuationAmount, controlIteration, restartFile
         ! Update the cost functional.
         if (.not. region%simulationFlags%predictionOnly) then
            instantaneousCostFunctional = functional%compute(region)
-           costFunctional = costFunctional +                                                 &
+           functional%runningTimeQuadrature = functional%runningTimeQuadrature +             &
                 timeIntegrator%norm(i) * timeStepSize * instantaneousCostFunctional
         end if
 
@@ -712,6 +713,9 @@ function runForward(this, region, actuationAmount, controlIteration, restartFile
      end do
      call region%saveData(QOI_TIME_AVERAGED_STATE, trim(this%outputPrefix) // ".mean.q")
   end if
+
+  if (.not. region%simulationFlags%predictionOnly)                                           &
+       costFunctional = functional%runningTimeQuadrature
 
   call endTiming("runForward")
 
@@ -854,7 +858,7 @@ function runAdjoint(this, region) result(costSensitivity)
 
         ! Update cost sensitivity.
         instantaneousCostSensitivity = controller%computeSensitivity(region)
-        costSensitivity = costSensitivity +                                                  &
+        controller%runningTimeQuadrature = controller%runningTimeQuadrature +                &
              timeIntegrator%norm(i) * timeStepSize * instantaneousCostSensitivity
 
         ! Update adjoint forcing on cost target patches.
@@ -897,6 +901,8 @@ function runAdjoint(this, region) result(costSensitivity)
 
   call this%residualManager%cleanup()
   call reverseMigratorFactory%cleanup()
+
+  costSensitivity = controller%runningTimeQuadrature
 
   call endTiming("runAdjoint")
 
