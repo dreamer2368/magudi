6d2a504a30cb023df52cc2e0efa907162968b455
David Buchta <dbuchta@chama-login7.sandia.gov>
2015-08-17 07:40:20 -0600

    updates to example

--- examples/WavyWallAcousticMonopole/chama.sh
+++ examples/WavyWallAcousticMonopole/chama.sh
@@ -26,44 +26,22 @@ PREFIX=WavyWallAcousticMonopole
 rm -f ${PREFIX}* 
 python config.py
 
-DIR=1em8
-AMP=1.e-8
-mkdir $DIR
-cp ${PREFIX}* *.inp $DIR/
-cd $DIR
-ln -sf /gscratch/dbuchta/magudi_target/magudi ./
-setOption "cost_functional_wallPenalty" "${AMP}"
-${MAGUDI_MPIRUN} ../magudi
-cd ../
-
-DIR=1em4
-AMP=1.e-4
-mkdir $DIR
-cp ${PREFIX}* *.inp $DIR/
-cd $DIR
-ln -sf /gscratch/dbuchta/magudi_target/magudi ./
-setOption "cost_functional_wallPenalty" "${AMP}"
-${MAGUDI_MPIRUN} ../magudi
-cd ../
 
 
-DIR=1em2
-AMP=1.e-2
-mkdir $DIR
-cp ${PREFIX}* *.inp $DIR/
-cd $DIR
-ln -sf /gscratch/dbuchta/magudi_target/magudi ./
-setOption "cost_functional_wallPenalty" "${AMP}"
-${MAGUDI_MPIRUN} ../magudi
-cd ../
+DIR=("1em16" "1em8" "1em4" "1em3" "1em2" "1em1")
+AMP=("1.e-16" "1.e-8" "1.e-4" "1.e-3" "1.e-2" "1.e-1")
 
-DIR=1em1
-AMP=1.e-1
-mkdir $DIR
-cp ${PREFIX}* *.inp $DIR/
-cd $DIR
-ln -sf /gscratch/dbuchta/magudi_target/magudi ./
-setOption "cost_functional_wallPenalty" "${AMP}"
-${MAGUDI_MPIRUN} ../magudi
-cd ../
+#DIR=("1em2" "1em1")
+#AMP=("1.e-2" "1.e-1")
+for i in "${!DIR[@]}"; do 
+     rm -rf ${DIR[$i]}
+	mkdir ${DIR[$i]}
+	cp ${PREFIX}* *.inp ${DIR[$i]}/
+	cd ${DIR[$i]}
+	ln -sf /gscratch/dbuchta/magudi_target/magudi ./
+	setOption "cost_functional_wallPenalty" "${AMP[$i]}"
+	${MAGUDI_MPIRUN} ../magudi
+	cd ../
+done
+
 
