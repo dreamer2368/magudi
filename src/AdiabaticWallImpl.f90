27a506cc661b1b1256c0a836693b299890a3c4db
Jesse Capecelatro <jessecaps@gmail.com>
2016-01-13 11:01:32 -0600

    First pass at an adiabatic wall. I think this is correct. Applied Neumann condition to the species as well. Haven't implemented the adjoint yet.

--- src/AdiabaticWallImpl.f90
+++ src/AdiabaticWallImpl.f90
@@ -36,17 +36,19 @@ subroutine setupAdiabaticWall(this, index, comm, patchDescriptor,               
 
   ! Viscous penalty amounts.
   if (simulationFlags%viscosityOn) then
+     this%viscousPenaltyAmounts(1) = getOption("defaults/viscous_penalty_amount", 1.0_wp)
+     this%viscousPenaltyAmounts(2) = 0.0_wp
      do i = 1, 2
         write(key, '(A,I0.0)') "patches/" // trim(patchDescriptor%name) //                   &
              "/viscous_penalty_amount", i
-        this%viscousPenaltyAmounts(i) = getOption(trim(key), 1.0_wp)
-        this%viscousPenaltyAmounts(i) = this%viscousPenaltyAmounts(i) *                      &
-             solverOptions%reynoldsNumberInverse
-        this%viscousPenaltyAmounts(i) = sign(this%viscousPenaltyAmounts(i),                  &
-             real(this%normalDirection, wp))
+        this%viscousPenaltyAmounts(i) = getOption(trim(key), this%viscousPenaltyAmounts(i))
         this%viscousPenaltyAmounts(i) = this%viscousPenaltyAmounts(i) /                      &
              grid%firstDerivative(abs(this%normalDirection))%normBoundary(1)
      end do
+     this%viscousPenaltyAmounts(1) = this%viscousPenaltyAmounts(1) *                         &
+          solverOptions%reynoldsNumberInverse
+     this%viscousPenaltyAmounts(2) = sign(this%viscousPenaltyAmounts(2),                     &
+          real(this%normalDirection, wp))
   else
      this%viscousPenaltyAmounts = 0.0_wp
   end if
@@ -95,8 +97,13 @@ subroutine addAdiabaticWallPenalty(this, mode, simulationFlags, solverOptions, g
 
   ! <<< Local variables >>>
   integer, parameter :: wp = SCALAR_KIND
-  integer :: i, j, k, nDimensions, nUnknowns, direction, gridIndex, patchIndex
-  SCALAR_TYPE, allocatable :: metricsAlongNormalDirection(:), viscousPenalties(:,:)
+  integer :: i, j, k, nDimensions, nSpecies, nUnknowns, direction, gridIndex, patchIndex
+  SCALAR_TYPE :: localTemperature
+  SCALAR_TYPE, allocatable :: metricsAlongNormalDirection(:), velocity(:,:), temperature(:), &
+       metrics(:,:), penaltyAtBoundary(:), penaltyNearBoundary(:,:), temp(:,:),              &
+       localVelocity(:), localMassFraction(:), localFluxJacobian(:,:), adjointPenalties(:,:),&
+       dynamicViscosity(:), secondCoefficientOfViscosity(:), thermalDiffusivity(:),          &
+       massDiffusivity(:,:)
 
   assert_key(mode, (FORWARD, ADJOINT))
   assert(this%gridIndex == grid%index)
@@ -118,11 +125,93 @@ subroutine addAdiabaticWallPenalty(this, mode, simulationFlags, solverOptions, g
   direction = abs(this%normalDirection)
   assert(direction >= 1 .and. direction <= nDimensions)
 
+  nSpecies = solverOptions%nSpecies
+  assert(nSpecies >= 0)
+
   nUnknowns = solverOptions%nUnknowns
-  assert(nUnknowns >= nDimensions + 2)
+  assert(nUnknowns == nDimensions + 2 + nSpecies)
+
+  if (this%nPatchPoints > 0) then
+
+     allocate(temperature(this%nPatchPoints))
+     allocate(dynamicViscosity(this%nPatchPoints))
+     allocate(secondCoefficientOfViscosity(this%nPatchPoints))
+     allocate(thermalDiffusivity(this%nPatchPoints))
+     if (nSpecies > 0) allocate(massDiffusivity(this%nPatchPoints, nSpecies))
+
+     call this%collect(state%temperature(:,1), temperature)
+
+     call computeTransportVariables(solverOptions%nSpecies, temperature,                     &
+          solverOptions%powerLawExponent, solverOptions%bulkViscosityRatio,                  &
+          solverOptions%ratioOfSpecificHeats, solverOptions%reynoldsNumberInverse,           &
+          solverOptions%prandtlNumberInverse, solverOptions%schmidtNumberInverse,            &
+          dynamicViscosity, secondCoefficientOfViscosity, thermalDiffusivity, massDiffusivity)
+
+     select case (mode)
+
+     case (FORWARD)
+
+        allocate(penaltyNearBoundary(grid%nGridPoints, nUnknowns - 1))
+        allocate(temp(this%nPatchPoints, nUnknowns - 1))
+        allocate(velocity(this%nPatchPoints, nDimensions))
+        allocate(metrics(this%nPatchPoints, nDimensions))
+
+        call this%collect(state%velocity, velocity)
+        call this%collect(grid%metrics(:,1+nDimensions*(direction-1):nDimensions*direction), &
+             metrics)
+
+        do i = 1, nDimensions
+           temp(:,i) = dynamicViscosity * velocity(:,i) * sum(metrics * metrics) +           &
+                (dynamicViscosity + secondCoefficientOfViscosity) *                          &
+                sum(velocity * metrics, 2) * metrics(:,i)
+        end do
+        temp(:,nDimensions+1:nUnknowns-1) = 0.0_wp
+        if (allocated(this%hole)) then
+           do i = 1, size(temp, 2)
+              where (this%hole == 1) temp(:, i) = 0.0_wp
+           end do
+        end if
+
+        call this%disperse(temp, penaltyNearBoundary)
+
+        do i = 1, size(penaltyNearBoundary, 2)
+           penaltyNearBoundary(:,i) = grid%jacobian(:,1) * penaltyNearBoundary(:,i)
+        end do
+        call grid%adjointFirstDerivative(direction)%projectOnBoundaryAndApply(               &
+             penaltyNearBoundary, grid%localSize, this%normalDirection)
+        call grid%firstDerivative(direction)%applyNorm(penaltyNearBoundary, grid%localSize)
+        do i = 1, size(penaltyNearBoundary, 2)
+           penaltyNearBoundary(:,i) = grid%jacobian(:,1) * penaltyNearBoundary(:,i)
+        end do
+
+        state%rightHandSide(:,2:nUnknowns) = state%rightHandSide(:,2:nUnknowns) -            &
+             this%viscousPenaltyAmounts(2) * penaltyNearBoundary
+
+        SAFE_DEALLOCATE(metrics)
+        SAFE_DEALLOCATE(velocity)
+        SAFE_DEALLOCATE(temp)
+        SAFE_DEALLOCATE(penaltyNearBoundary)
+
+     case (ADJOINT)
+
+        allocate(temp(grid%nGridPoints, nUnknowns - 1))
+
+        temp = state%adjointVariables(:,2:nUnknowns)
+        call grid%firstDerivative(direction)%applyAndProjectOnBoundary(temp, grid%localSize, &
+             this%normalDirection)
+
+     end select
+
+  end if
 
   allocate(metricsAlongNormalDirection(nDimensions))
-  allocate(viscousPenalties(nUnknowns - 1, 2))
+  allocate(penaltyAtBoundary(nUnknowns))
+  if (mode == ADJOINT) then
+     allocate(adjointPenalties(nUnknowns, 2))
+     allocate(localVelocity(nDimensions))
+     allocate(localFluxJacobian(nUnknowns - 1, nUnknowns - 1))
+     if (nSpecies > 0) allocate(localMassFraction(nSpecies))
+  end if
 
   do k = this%offset(3) + 1, this%offset(3) + this%localSize(3)
      do j = this%offset(2) + 1, this%offset(2) + this%localSize(2)
@@ -134,26 +223,66 @@ subroutine addAdiabaticWallPenalty(this, mode, simulationFlags, solverOptions, g
            patchIndex = i - this%offset(1) + this%localSize(1) *                             &
                 (j - 1 - this%offset(2) + this%localSize(2) *                                &
                 (k - 1 - this%offset(3)))
+           if (allocated(this%hole)) then
+              if (this%hole(patchIndex) == 1) cycle
+           end if
 
            metricsAlongNormalDirection =                                                     &
                 grid%metrics(gridIndex,1+nDimensions*(direction-1):nDimensions*direction)
-
-           viscousPenalties(:,:) = 0.0_wp !... TODO: implement an adiabatic wall SAT
-
-           viscousPenalties = grid%jacobian(gridIndex, 1) * viscousPenalties
 
            select case (mode)
 
            case (FORWARD)
 
-              state%rightHandSide(gridIndex,2:nUnknowns) =                                   &
-                   state%rightHandSide(gridIndex,2:nUnknowns) -                              &
-                   this%viscousPenaltyAmounts(1) * viscousPenalties(:,1) +                   &
-                   this%viscousPenaltyAmounts(2) * viscousPenalties(:,2)
+              penaltyAtBoundary(1) = 0.0_wp
+              penaltyAtBoundary(2:nDimensions+1) =                                           &
+                   state%conservedVariables(gridIndex,2:nDimensions+1)
+              penaltyAtBoundary(nDimensions+2:nUnknowns) = 0.0_wp
+              penaltyAtBoundary = grid%jacobian(gridIndex, 1) * penaltyAtBoundary
+
+              state%rightHandSide(gridIndex,:) = state%rightHandSide(gridIndex,:) -          &
+                   this%viscousPenaltyAmounts(1) * penaltyAtBoundary
 
            case (ADJOINT)
 
-              ! TODO: add viscous wall penalties for adjoint variables.
+              ! TODO: add adjoint penalties for the species.
+
+              localVelocity = state%velocity(gridIndex,:)
+              localTemperature = state%temperature(gridIndex,1)
+              if (nSpecies > 0) localMassFraction = state%massFraction(gridIndex,:)
+
+              adjointPenalties(1,1) = 0.0_wp
+              adjointPenalties(2:nDimensions+2,1) =                                          &
+                   state%adjointVariables(gridIndex,2:nDimensions+2)
+
+              call computeSecondPartialViscousJacobian(nDimensions, nSpecies,                &
+                   solverOptions%equationOfState, localVelocity,                             &
+                   dynamicViscosity(patchIndex),                                             &
+                   secondCoefficientOfViscosity(patchIndex),                                 &
+                   thermalDiffusivity(patchIndex), massDiffusivity(patchIndex,:),            &
+                   temperature(patchIndex), solverOptions%schmidtNumberInverse,              &
+                   solverOptions%molecularWeightInverse, grid%jacobian(gridIndex,1),         &
+                   metricsAlongNormalDirection, metricsAlongNormalDirection,                 &
+                   localFluxJacobian)
+
+              adjointPenalties(2:nUnknowns,2) = matmul(transpose(localFluxJacobian),         &
+                   temp(gridIndex,:))
+
+              adjointPenalties(nDimensions+2,2) = solverOptions%ratioOfSpecificHeats *       &
+                   state%specificVolume(gridIndex,1) * adjointPenalties(nDimensions+2,2)
+              adjointPenalties(2:nDimensions+1,2) = state%specificVolume(gridIndex,1) *      &
+                   adjointPenalties(2:nDimensions+1,2) - localVelocity *                     &
+                   adjointPenalties(nDimensions+2,2)
+              adjointPenalties(1,2) = - state%specificVolume(gridIndex,1) *                  &
+                   state%conservedVariables(gridIndex,nDimensions+2) *                       &
+                   adjointPenalties(nDimensions+2,2) - sum(localVelocity *                   &
+                   adjointPenalties(2:nDimensions+1,2))
+
+              adjointPenalties = grid%jacobian(gridIndex, 1) * adjointPenalties
+
+              state%rightHandSide(gridIndex,:) = state%rightHandSide(gridIndex,:) +          &
+                   this%viscousPenaltyAmounts(1) * adjointPenalties(:,1) -                   &
+                   this%viscousPenaltyAmounts(2) * adjointPenalties(:,2)
 
            end select !... mode
 
@@ -161,8 +290,17 @@ subroutine addAdiabaticWallPenalty(this, mode, simulationFlags, solverOptions, g
      end do !... j = this%offset(2) + 1, this%offset(2) + this%localSize(2)
   end do !... k = this%offset(3) + 1, this%offset(3) + this%localSize(3)
 
-  SAFE_DEALLOCATE(viscousPenalties)
+  SAFE_DEALLOCATE(massDiffusivity)
+  SAFE_DEALLOCATE(thermalDiffusivity)
+  SAFE_DEALLOCATE(dynamicViscosity)
+  SAFE_DEALLOCATE(secondCoefficientOfViscosity)
+  SAFE_DEALLOCATE(temperature)
+  SAFE_DEALLOCATE(localFluxJacobian)
+  SAFE_DEALLOCATE(localMassFraction)
+  SAFE_DEALLOCATE(localVelocity)
+  SAFE_DEALLOCATE(adjointPenalties)
   SAFE_DEALLOCATE(metricsAlongNormalDirection)
+  SAFE_DEALLOCATE(temp)
 
   call endTiming("addAdiabaticWallPenalty")
 
