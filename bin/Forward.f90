9c1b958329ea769b21b201b78cfe98c659743c93
Seung Whan Chung <chung28@quartz1922.llnl.gov>
2018-05-08 11:43:31 -0700

    SeungWhan: binary files.

---
+++ bin/Forward.f90
@@ -0,0 +1,122 @@
+#include "config.h"
+
+program forward
+
+  use MPI
+  use, intrinsic :: iso_fortran_env, only : output_unit
+
+  use Region_mod, only : t_Region
+  use Solver_mod, only : t_Solver
+
+  use Grid_enum
+  use State_enum
+
+  use InputHelper, only : parseInputFile, getOption, getRequiredOption
+  use ErrorHandler
+  use PLOT3DHelper, only : plot3dDetectFormat, plot3dErrorMessage
+  use MPITimingsHelper, only : startTiming, endTiming, reportTimings, cleanupTimers
+
+  implicit none
+
+  integer, parameter :: wp = SCALAR_KIND
+  integer :: i, procRank, numProcs, ierror
+  character(len = STRING_LENGTH) :: filename, outputPrefix, message
+  logical :: success
+  integer, dimension(:,:), allocatable :: globalGridSizes
+  type(t_Region) :: region
+  type(t_Solver) :: solver
+  SCALAR_TYPE :: dummyValue
+
+  ! Initialize MPI.
+  call MPI_Init(ierror)
+  call MPI_Comm_rank(MPI_COMM_WORLD, procRank, ierror)
+  call MPI_Comm_size(MPI_COMM_WORLD, numProcs, ierror)
+
+  call initializeErrorHandler()
+
+  if (command_argument_count() > 1) then
+     write(message, '(A)') "Usage: magudi [FILE]"
+     call writeAndFlush(MPI_COMM_WORLD, output_unit, message)
+     write(message, '(A)') "High-performance Fortran-based adjoint optimization tool."
+     call writeAndFlush(MPI_COMM_WORLD, output_unit, message)
+     write(message, '(A)')                                                                   &
+          "FILE is an optional restart file used if running in prediction-only mode."
+     call writeAndFlush(MPI_COMM_WORLD, output_unit, message)
+     call cleanupErrorHandler()
+     call MPI_Finalize(ierror)
+     stop -1
+  end if
+
+  call startTiming("total")
+
+  ! Parse options from the input file.
+  filename = PROJECT_NAME // ".inp"
+  call parseInputFile(filename)
+
+  outputPrefix = getOption("output_prefix", PROJECT_NAME)
+
+  ! Verify that the grid file is in valid PLOT3D format and fetch the grid dimensions:
+  ! `globalGridSizes(i,j)` is the number of grid points on grid `j` along dimension `i`.
+  call getRequiredOption("grid_file", filename)
+  call plot3dDetectFormat(MPI_COMM_WORLD, filename, success,                                 &
+       globalGridSizes = globalGridSizes)
+  if (.not. success) call gracefulExit(MPI_COMM_WORLD, plot3dErrorMessage)
+
+  ! Setup the region.
+  call region%setup(MPI_COMM_WORLD, globalGridSizes)
+
+  ! Read the grid file.
+  call getRequiredOption("grid_file", filename)
+  call region%loadData(QOI_GRID, filename)
+
+  ! Update the grids by computing the Jacobian, metrics, and norm.
+  do i = 1, size(region%grids)
+     call region%grids(i)%update()
+  end do
+  call MPI_Barrier(region%comm, ierror)
+
+  ! Write out some useful information.
+  call region%reportGridDiagnostics()
+
+  ! Save the Jacobian and normalized metrics.
+  write(filename, '(2A)') trim(outputPrefix), ".Jacobian.f"
+  call region%saveData(QOI_JACOBIAN, filename)
+  write(filename, '(2A)') trim(outputPrefix), ".metrics.f"
+  call region%saveData(QOI_METRICS, filename)
+
+  ! Initialize the solver.
+  call solver%setup(region, outputPrefix = outputPrefix)
+
+  ! Save the control and target mollifier if using code-generated values.
+  if (.not. region%simulationFlags%predictionOnly) then
+     filename = getOption("control_mollifier_file", "")
+     if (len_trim(filename) == 0) call region%saveData(QOI_CONTROL_MOLLIFIER,                &
+          trim(outputPrefix) // ".control_mollifier.f")
+     filename = getOption("target_mollifier_file", "")
+     if (len_trim(filename) == 0) call region%saveData(QOI_TARGET_MOLLIFIER,                 &
+          trim(outputPrefix) // ".target_mollifier.f")
+  end if
+
+  ! Main code logic.
+  if (command_argument_count() == 1) then
+     call get_command_argument(1, filename)
+     dummyValue = solver%runForward(region, restartFilename = filename,                   &
+                                     actuationAmount =                                    &
+                                         getOption("actuation_amount", 0.0_wp))
+  else
+     dummyValue = solver%runForward(region, actuationAmount =                             &
+                                         getOption("actuation_amount", 0.0_wp))
+  end if
+
+  call solver%cleanup()
+  call region%cleanup()
+
+  call endTiming("total")
+  call reportTimings()
+  call cleanupTimers()
+
+  ! Finalize MPI.
+  call cleanupErrorHandler()
+  call MPI_Finalize(ierror)
+
+end program forward
