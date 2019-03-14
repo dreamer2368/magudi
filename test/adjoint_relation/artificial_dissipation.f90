e39500980b2610e302918974908105ee01dfc285
Kevin SeungWhan Chung <schung58@illinois.edu>
2019-03-14 00:40:38 -0500

    adjoint relation test: artificial dissipation.

---
+++ test/adjoint_relation/artificial_dissipation.f90
@@ -0,0 +1,396 @@
+#include "config.h"
+
+program artificial_dissipation
+
+  use MPI
+
+  use CNSHelper
+  use ErrorHandler, only : initializeErrorHandler, cleanupErrorHandler
+  use RandomNumber, only : initializeRandomNumberGenerator, random
+
+  implicit none
+
+  logical :: success, success_, isPeriodic
+  integer :: i, j, k, nDimensions, ierror
+  integer :: procRank
+  character(len = STRING_LENGTH), parameter :: discretizationTypes(4) =                      &
+       (/ "SBP 1-2", "SBP 2-4", "SBP 3-6", "SBP 4-8" /)
+
+  interface
+
+     real(SCALAR_KIND) function meanTrimmed(a)
+       real(SCALAR_KIND), intent(inout) :: a(:)
+     end function meanTrimmed
+
+     subroutine sort(a)
+       real(SCALAR_KIND), intent(inout) :: a(:)
+     end subroutine sort
+
+     subroutine testAdjointRelation(identifier, nDimensions, success, isPeriodic, tolerance)
+
+       character(len = *), intent(in) :: identifier
+       integer, intent(in) :: nDimensions
+       logical, intent(out) :: success
+
+       logical, intent(in), optional :: isPeriodic
+       real(SCALAR_KIND), intent(in), optional :: tolerance
+
+     end subroutine testAdjointRelation
+
+  end interface
+
+  call MPI_Init(ierror)
+  call MPI_Comm_rank(MPI_COMM_WORLD, procRank, ierror)
+
+  success = .true.
+
+  call initializeErrorHandler()
+  call initializeRandomNumberGenerator()
+
+  do nDimensions = 1, 3
+    do j = 1, 4 !... for each discretizationTypes
+      success = .true.
+      do i = 1, 10 !... test multiple times
+        isPeriodic = .true.
+        call testAdjointRelation(discretizationTypes(j), nDimensions,           &
+                                 success_, isPeriodic)
+        success = success .and. success_
+        if( .not. success) then
+          if( procRank == 0 ) then
+            print *, 'Failed, ', trim(discretizationTypes(j))
+            print *, 'dimension: ', nDimensions
+            print *, 'periodicity: ', isPeriodic
+          end if
+          exit
+        end if
+
+        isPeriodic = .false.
+        call testAdjointRelation(discretizationTypes(j), nDimensions,           &
+                                 success_, isPeriodic)
+        success = success .and. success_
+        if( .not. success_) then
+          if( procRank == 0 ) then
+            print *, 'Failed, ', trim(discretizationTypes(j))
+            print *, 'dimension: ', nDimensions
+            print *, 'periodicity: ', isPeriodic
+          end if
+          exit
+        end if
+      end do
+      if( procRank == 0 .and. success ) then
+        print *, 'Success, ', trim(discretizationTypes(j))
+        print *, 'dimension: ', nDimensions
+      end if
+    end do
+  end do
+
+  call cleanupErrorHandler()
+
+  call MPI_Allreduce(MPI_IN_PLACE, success, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierror)
+  call MPI_Finalize(ierror)
+  if (.not. success) stop -1
+  stop 0
+
+end program artificial_dissipation
+
+subroutine sort(a)
+
+  ! <<< Arguments >>>
+  real(SCALAR_KIND), intent(inout) :: a(:)
+
+  ! <<< Local variables >>>
+  integer :: i, j
+  real(SCALAR_KIND) :: temp
+
+  do i = 2, size(a)
+     j = i - 1
+     temp = a(i)
+     do while (a(j) > temp)
+        a(j+1) = a(j)
+        j = j - 1
+        if (j < 1) exit
+     end do
+     a(j+1) = temp
+  end do
+
+end subroutine sort
+
+real(SCALAR_KIND) function median(a)
+
+  ! <<< Arguments >>>
+  real(SCALAR_KIND), intent(in) :: a(:)
+
+  ! <<< Local variables >>>
+  integer, parameter :: wp = SCALAR_KIND
+  integer :: n
+
+  n = size(a)
+  if (mod(n, 2) == 1) then
+     median = a((n + 1) / 2)
+  else
+     median = 0.5_wp * (a(n / 2) + a(n / 2 + 1))
+  end if
+
+end function median
+
+real(SCALAR_KIND) function meanTrimmed(a)
+
+  ! <<< Arguments >>>
+  real(SCALAR_KIND), intent(inout) :: a(:)
+
+  ! <<< Scalar variables >>>
+  integer, parameter :: wp = SCALAR_KIND
+  integer :: i, n
+  real(wp) :: firstQuartile, thirdQuartile
+
+  interface
+     real(SCALAR_KIND) function median(a)
+       real(SCALAR_KIND), intent(in) :: a(:)
+     end function median
+  end interface
+
+  n = size(a)
+
+  if (mod(n, 2) == 0) then
+     firstQuartile = median(a(1:n/2))
+     thirdQuartile = median(a(n/2+1:n))
+  else
+     firstQuartile = median(a(1:(n-1)/2))
+     thirdQuartile = median(a((n+1)/2+1:n))
+  end if
+
+  meanTrimmed = 0.0_wp
+  n = 0
+
+  do i = 1, size(a)
+     if (a(i) >= firstQuartile .and. a(i) <= thirdQuartile) then
+        meanTrimmed = meanTrimmed + a(i)
+        n = n + 1
+     end if
+  end do
+
+  if (n == 0) return
+  meanTrimmed = meanTrimmed / real(n, wp)
+
+end function meanTrimmed
+
+subroutine testAdjointRelation(identifier, nDimensions, success, isPeriodic, tolerance)
+
+  ! <<< External modules >>>
+  use MPI
+
+  ! <<< Derived types >>>
+  use StencilOperator_mod, only : t_StencilOperator
+  use Grid_mod, only : t_Grid
+  use State_mod, only : t_State
+  use SolverOptions_mod, only : t_SolverOptions
+  use SimulationFlags_mod, only : t_SimulationFlags
+
+  use Region_enum, only : FORWARD, ADJOINT
+
+  use CNSHelper
+  use RhsHelperImpl, only : addDissipation
+
+  ! <<< Internal modules >>>
+  use MPIHelper, only : pigeonhole
+  use RandomNumber, only : random
+
+  ! <<< Arguments >>>
+  character(len = *), intent(in) :: identifier
+  integer, intent(in) :: nDimensions
+  logical, intent(out) :: success
+  logical, intent(in), optional :: isPeriodic
+  real(SCALAR_KIND), intent(in), optional :: tolerance
+
+  ! <<< interface >>>
+  interface
+     real(SCALAR_KIND) function meanTrimmed(a)
+       real(SCALAR_KIND), intent(inout) :: a(:)
+     end function meanTrimmed
+
+     subroutine sort(a)
+       real(SCALAR_KIND), intent(inout) :: a(:)
+     end subroutine sort
+  end interface
+
+  ! <<< Local derived type variables >>>
+  type(t_SimulationFlags) :: simulationFlags
+  type(t_SolverOptions) :: solverOptions
+  type(t_Grid) :: grid
+  type(t_State) :: state0, state1
+
+  ! <<< Local variables >>>
+  integer, parameter :: wp = SCALAR_KIND
+  logical :: isPeriodic_(3), hasNegativeJacobian
+  real(wp) :: scalar1, scalar2, tolerance_,                                           &
+              stepSizes(32), errorHistory(32), convergenceHistory(31)
+  integer :: i, j, k, gridSize(3, 1), nUnknowns
+  real(SCALAR_KIND), allocatable :: F(:,:),                                           &
+                                    adjointRightHandSide(:,:),                        &
+                                    deltaConservedVariables(:,:), deltaPrimitiveVariables(:,:),&
+                                    temp2(:,:)
+  character(len = STRING_LENGTH) :: errorMessage
+
+  tolerance_ = 1.0E-10
+  if( present(tolerance) ) tolerance_ = tolerance
+
+  success = .true.
+
+  ! set up simulation flags
+  call simulationFlags%initialize()
+  simulationFlags%enableController = .true.
+  simulationFlags%enableFunctional = .true.
+  simulationFlags%enableAdjoint = .true.
+  ! randomize curvilinear domain
+  ! simulationFlags%isDomainCurvilinear = (random(0, 2) == 0)
+  simulationFlags%isDomainCurvilinear = .false.
+  simulationFlags%dissipationOn = .true.
+  simulationFlags%compositeDissipation = .false. !(random(0,1)==0)
+  if( trim(identifier)=="SBP 1-2" ) simulationFlags%compositeDissipation = .true.
+
+  ! randomize grid size
+  ! Note that too small grid size will yield null matrix for stencil operators.
+  gridSize(:,:) = 1
+  do i = 1, nDimensions
+     gridSize(i,1) = random(20, 40)
+  end do
+
+  ! initialize solver option, grid, and states
+  simulationFlags%dissipationOn = .false. !...only for solverOptions initialization
+  call solverOptions%initialize(nDimensions, simulationFlags)
+  solverOptions%discretizationType = trim(identifier)
+  simulationFlags%dissipationOn = .true.
+  solverOptions%dissipationAmount = random(0.01_wp,1.0_wp)
+  call grid%setup(1, gridSize(1:nDimensions,1), MPI_COMM_WORLD,                        &
+       simulationFlags = simulationFlags)
+  call grid%setupSpatialDiscretization(simulationFlags, solverOptions)
+  ! grid is not randomized: do not put any argument in updateGrid!!
+  call grid%update()
+
+  call state0%setup(grid, simulationFlags, solverOptions)
+  call state1%setup(grid, simulationFlags, solverOptions)
+
+  ! Randomize conserved variables.
+  do i = 1, grid%nGridPoints
+     state0%conservedVariables(i,1) = random(0.01_wp, 10.0_wp)
+     do j = 1, nDimensions
+        state0%conservedVariables(i,j+1) =                                              &
+             state0%conservedVariables(i,1) * random(-10.0_wp, 10.0_wp)
+     end do
+     state0%conservedVariables(i, nDimensions + 2) = state0%conservedVariables(i,1) *    &
+          random(0.01_wp, 10.0_wp) / solverOptions%ratioOfSpecificHeats +              &
+          0.5_wp / state0%conservedVariables(i,1) *                                     &
+          sum(state0%conservedVariables(i,2:nDimensions+1) ** 2)
+  end do
+  assert(all(state0%conservedVariables(:,1) > 0.0_wp))
+
+  ! Randomize adjoint variables.
+  allocate(F(grid%nGridPoints, solverOptions%nUnknowns))
+  call random_number(F)
+  state0%adjointVariables = F
+  SAFE_DEALLOCATE(F)
+
+  ! Randomize delta conserved variables.
+  allocate(deltaConservedVariables(grid%nGridPoints, solverOptions%nUnknowns))
+  allocate(deltaPrimitiveVariables(grid%nGridPoints, solverOptions%nUnknowns))
+  do i = 1, grid%nGridPoints
+    do j = 1, nDimensions + 2
+       deltaPrimitiveVariables(i,j) = random(-1.0_wp, 1.0_wp)
+    end do
+  end do
+  deltaConservedVariables(:,1) = deltaPrimitiveVariables(:,1)
+  do j = 1, nDimensions
+     deltaConservedVariables(:,j+1) = state0%conservedVariables(:,j+1) /                     &
+          state0%conservedVariables(:,1) * deltaPrimitiveVariables(:,1) +                    &
+          state0%conservedVariables(:,1) * deltaPrimitiveVariables(:,j+1)
+  end do
+  deltaConservedVariables(:,nDimensions+2) = state0%conservedVariables(:,nDimensions+2) /    &
+       state0%conservedVariables(:,1) * deltaPrimitiveVariables(:,1) +                       &
+       sum(state0%conservedVariables(:,2:nDimensions+1) *                                    &
+       deltaPrimitiveVariables(:,2:nDimensions+1), dim = 2) +                          &
+       state0%conservedVariables(:,1) / solverOptions%ratioOfSpecificHeats *                               &
+       deltaPrimitiveVariables(:,nDimensions+2)
+
+  ! Compute baseline dissipation
+  state0%rightHandSide = 0.0_wp
+  ! (1) add dissipation
+  if (simulationFlags%dissipationOn)                                                         &
+       call addDissipation(FORWARD, simulationFlags, solverOptions, grid, state0)
+  ! (2) Multiply by Jacobian
+  do j = 1, solverOptions%nUnknowns
+     state0%rightHandSide(:,j) = state0%rightHandSide(:,j) * grid%jacobian(:,1)
+  end do
+
+  ! Compute adjoint rhs for dissipation
+  nUnknowns = solverOptions%nUnknowns
+  allocate(adjointRightHandSide(grid%nGridPoints, solverOptions%nUnknowns))
+  allocate(temp2(grid%nGridPoints, solverOptions%nUnknowns))
+  temp2 = state0%rightHandSide
+  state0%rightHandSide = 0.0_wp
+  ! (1) add dissipation
+  if (simulationFlags%dissipationOn)                                                         &
+       call addDissipation(ADJOINT, simulationFlags, solverOptions, grid, state0)
+  adjointRightHandSide = state0%rightHandSide
+  state0%rightHandSide = temp2
+  SAFE_DEALLOCATE(temp2)
+  ! (2) Multiply by Jacobian
+  do j = 1, solverOptions%nUnknowns
+     adjointRightHandSide(:,j) = adjointRightHandSide(:,j) * grid%jacobian(:,1)
+  end do
+
+  ! <R^{\dagger}u, \delta v>
+  scalar1 = grid%computeInnerProduct(adjointRightHandSide,deltaConservedVariables)
+  ! <u, \delta R(v)>
+  ! Prepare step sizes
+  stepSizes(1) = 0.01_wp
+  do k = 2, size(stepSizes)
+     stepSizes(k) = stepSizes(k-1) * 10.0_wp**(-0.25_wp)
+  end do
+  errorHistory = 0.0_wp
+  convergenceHistory = 0.0_wp
+  do k = 1, size(stepSizes)
+    !(1) finite difference on conserved variables
+    state1%conservedVariables = state0%conservedVariables + stepSizes(k) * deltaConservedVariables
+
+    ! (2)Compute baseline dissipation
+    state1%rightHandSide = 0.0_wp
+    ! (2-1) add dissipation
+    if (simulationFlags%dissipationOn)                                                         &
+         call addDissipation(FORWARD, simulationFlags, solverOptions, grid, state1)
+    ! (2-2) Multiply by Jacobian
+    do j = 1, solverOptions%nUnknowns
+       state1%rightHandSide(:,j) = state1%rightHandSide(:,j) * grid%jacobian(:,1)
+    end do
+
+    ! (3) <u, \delta R(v)>
+    scalar2 = grid%computeInnerProduct(state0%adjointVariables,                             &
+                                        state1%rightHandSide - state0%rightHandSide)
+
+    errorHistory(k) = abs( (scalar2/stepSizes(k) + scalar1)/scalar1 )
+    ! print *, stepSizes(k), -scalar1, scalar2/stepSizes(k), errorHistory(k)
+
+    if (k > 1) then
+       convergenceHistory(k-1) = log(errorHistory(k) / errorHistory(k-1)) /              &
+            log(stepSizes(k) / stepSizes(k-1))
+       if (convergenceHistory(k-1) < 0.0_wp) exit
+    end if
+  end do
+
+  if (maxval(errorHistory).le.tolerance_) then
+    success = .true.
+  elseif (k > 3) then
+     call sort(convergenceHistory(:k-2))
+     success = success .and. nint(meanTrimmed(convergenceHistory(:k-2))).ge.1
+  else
+     success = .false.
+  end if
+
+  SAFE_DEALLOCATE(adjointRightHandSide)
+  SAFE_DEALLOCATE(deltaConservedVariables)
+  SAFE_DEALLOCATE(deltaPrimitiveVariables)
+
+  call state0%cleanup()
+  call state1%cleanup()
+  call grid%cleanup()
+
+end subroutine testAdjointRelation
